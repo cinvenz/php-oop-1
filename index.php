97d9c0602be09f9d5e851a6fb9d70fb66b7e042d
cinvenz <vincenzobortune@gmail.com>
2023-01-09 17:04:12 +0100

    finish

---
+++ index.php
@@ -0,0 +1,34 @@
+<?
+class Movie
+{
+    public $genre;
+    public $release;
+    public $studio;
+
+
+    public function __construct($genre, $release, $studio)
+    {
+        $this->genre = $genre;
+        $this->release = $release;
+        $this->studio = $studio;
+    }
+
+    public function setEta($release)
+    {
+        if (is_integer($release) && $release >= 0) {
+            $this->release = $release;
+        }
+        return $release;
+    }
+
+    public function getRelease()
+    {
+        return $this->release;
+    }
+}
+
+$avatar = new Movie('Fantasy', 2009, 'Paramount');
+$titanic = new Movie('Romance', 1997, 'Warner Bros');
+
+var_dump($avatar);
+var_dump($titanic);
